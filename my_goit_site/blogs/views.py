7398f6a546f8c474eb479c66002f37e6d2e6e487
zhendler <zhendlerbing@gmail.com>
2024-10-28 08:43:08 +0200

    19,19

--- my_goit_site/blogs/views.py
+++ my_goit_site/blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)  # автоматично вхід після реєстрації
+            return redirect('home')  # змініть 'home' на ваш основний шлях
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blogs/register.html', {'form': form})
